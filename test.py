b6d1b18dfcf020f31fb48d94378740dbd7f7d381
ellaj19 <ellaj19@nycstudents.net>
2025-09-30 10:13:10 -0400

    Update test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+CafeItemList = [
+        {"name": "Mia's boleens", "price": 15.99, "description": "A well-made collage of Mia's misspelled data types"},
+        {"name": "olivia's cat's fuzzy toes", "price": 20.99, "description": "a chance to feel the fuzzy little toes of Olivia Lu's cats, Ollie and Ellie. You have a choice between the chunky one and the skinny one."},
+        {"name": "Yakult Iced Coffee", "price": 6.99, "description": "a cool, refreshing coffee drink to cool you down on the worst and best of days."},
+        {"name": "Salted Caramel Hot Chocolate (with peppermint crumbs)", "price": 6.99, "description": "A warm, comforting hot chocolate infused with notes of salted caramel and peppermint undertones"},
+        {"name": "Strawberry Tiramisu (with a suprise bigback gooner fanfic)", "price": 12.99, "description": "A creamy, light dessert incorporating fresh strawberries, creamy marscapone, soft ladyfingers, and a generous douse of coffee. Comes with a book in collaboration with BigBack Liver's Library."}
+        ]
+
+print(CafeItemList["name"])
