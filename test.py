c90ea976000d0081b41517a8567366ceb4cd39a9
ellaj19 <ellaj19@nycstudents.net>
2025-10-01 10:22:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 CafeItemList = [
-        {"name": "Mia's boleens", "price": 15.99, "description": "A well-made collage of Mia's misspelled data types"},
-        {"name": "olivia's cat's fuzzy toes", "price": 20.99, "description": "a chance to feel the fuzzy little toes of Olivia Lu's cats, Ollie and Ellie. You have a choice between the chunky one and the skinny one."},
-        {"name": "Yakult Iced Coffee", "price": 6.99, "description": "a cool, refreshing coffee drink to cool you down on the worst and best of days."},
-        {"name": "Salted Caramel Hot Chocolate (with peppermint crumbs)", "price": 6.99, "description": "A warm, comforting hot chocolate infused with notes of salted caramel and peppermint undertones"},
-        {"name": "Strawberry Tiramisu (with a suprise bigback gooner fanfic)", "price": 12.99, "description": "A creamy, light dessert incorporating fresh strawberries, creamy marscapone, soft ladyfingers, and a generous douse of coffee. Comes with a book in collaboration with BigBack Liver's Library."}
+        {"name": "1. Mia's boleens", "price": 15.99, "description": "A well-made collage of Mia's misspelled data types"},
+        {"name": "2. olivia's cat's fuzzy toes", "price": 20.99, "description": "a chance to feel the fuzzy little toes of Olivia Lu's cats, Ollie and Ellie. You have a choice between the chunky one and the skinny one."},
+        {"name": "3. Yakult Iced Coffee", "price": 6.99, "description": "a cool, refreshing coffee drink to cool you down on the worst and best of days."},
+        {"name": "4. Salted Caramel Hot Chocolate (with peppermint crumbs)", "price": 6.99, "description": "A warm, comforting hot chocolate infused with notes of salted caramel and peppermint undertones"},
+        {"name": "5. Strawberry Tiramisu (with a suprise bigback gooner fanfic)", "price": 12.99, "description": "A creamy, light dessert incorporating fresh strawberries, creamy marscapone, soft ladyfingers, and a generous douse of coffee. Comes with a book in collaboration with BigBack Liver's Library."},
+        {"name": "6. maura's yaoi notebook", "price": 2.99, "description": "a magical notebook donated by my dear friend, filled with the tales of a faraway land that immediately transport you to a whole new dimension."},
+        {"name": "7. "}
         ]
 
-print(CafeItemList["name"])
+
+
+for i in range(0, len(CafeItemList)):
+    print(CafeItemList[i]["name"])
+    input(int("what would you like to buy?"))
