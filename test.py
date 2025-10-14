0b459007957194bf293c00ce07f6546498dd68be
ellaj19 <ellaj19@nycstudents.net>
2025-10-14 10:17:07 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 CafeItemList = [
-        {"name": "1. Mia's boleens", "price": 15.99, "description": "A well-made collage of Mia's misspelled data types"},
+        {"name": "1. Mia's booleens", "price": 15.99, "description": "A well-made collage of Mia's misspelled data types"},
         {"name": "2. olivia's cat's fuzzy toes", "price": 20.99, "description": "a chance to feel the fuzzy little toes of Olivia Lu's cats, Ollie and Ellie. You have a choice between the chunky one and the skinny one."},
         {"name": "3. Yakult Iced Coffee", "price": 6.99, "description": "a cool, refreshing coffee drink to cool you down on the worst and best of days."},
         {"name": "4. Salted Caramel Hot Chocolate (with peppermint crumbs)", "price": 6.99, "description": "A warm, comforting hot chocolate infused with notes of salted caramel and peppermint undertones"},
@@ -11,9 +11,11 @@ CafeItemList = [
 
 
 for i in range(0, len(CafeItemList)):
+    print(CafeItemList[0]["name"])
+Cart = input("what would you like to buy?")
+
+
     
-    int(input("what would you like to buy?"))
-    print(CafeItemList[i])
 
 
 
